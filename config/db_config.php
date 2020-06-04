51c122a9f30fff5c98d1261d231f748a8b5ad1da
Oleksander <badvaginka@gmail.com>
2020-06-05 01:42:32 +0200

    config

---
+++ config/db_config.php
@@ -0,0 +1,9 @@
+<?php
+  $db_settings = [
+    "host" => "localhost",
+    "user" => "goverment_user",
+    "password" => "goverment",
+    "db_name" => "cas"
+  ];
+  $api_valid_token = "Y29tcHV0ZXJfYWlkZWRfc3lzdGVt";
+ ?>
